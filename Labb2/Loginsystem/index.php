24a15bbe1c5760e4d5c9d77c95fc090fb4a951f8
Emil Dannberger <ed222an@student.lnu.se>
2014-09-19 21:20:08 +0200

    Session reg
    Added a session regeneration thingy.

--- Labb2/Loginsystem/index.php
+++ Labb2/Loginsystem/index.php
@@ -1,5 +1,7 @@
 <?php
 	session_start();
+	session_regenerate_id();
+	ini_set('session.cookie_httponly', true);
 	setlocale(LC_ALL , "swedish"); // Ställer in sidans format så att månad, år, tid etc. visas på svenska.
 	
 	require_once("../../common/HTMLView.php");
